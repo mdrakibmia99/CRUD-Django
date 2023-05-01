59622056aa2ddb3a44d87754b2b3eef065fd176e
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-05-01 20:11:13 +0600

    in models.py of APP folder  create a product model

--- App/models.py
+++ App/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    GENDER=(
+        ('M','M'),
+        ('F','F'),
+        ('U','U'),
+    )
+    id=models.IntegerField(primary_key=True)
+    product=models.CharField(max_length=40)
+    sale=models.CharField(max_length=10)
+    qry=models.CharField(max_length=10)
+    gender=models.CharField(max_length=1,null=True,choices=GENDER)
+    note=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.product
