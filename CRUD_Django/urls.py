1b5f6940f86a857c6a9e42c66c2d3f1d9952dbe1
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-05-01 20:23:33 +0600

    in urls.py file of CRUD_Django folder create urls for add delete edit view

--- CRUD_Django/urls.py
+++ CRUD_Django/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Home
+    path('',views.home,name="home"),
+    # add product
+    path('add_product',views.add_product,name="add_product"),
+    # view the product individually
+    path('product/<str:product_id>',views.product,name="product"),
+    # edit product
+    path('edit_product',views.edit_product,name="edit_product"),
+    # delete product
+    path('delete_product/<str:delete_product',views.delete_product,name="delete_product"),
 ]
