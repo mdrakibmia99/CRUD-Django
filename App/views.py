b771b79ecfae12dacaf722a7d58a436b1ddceef8
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-05-01 20:41:24 +0600

    for add product create a view

--- App/views.py
+++ App/views.py
@@ -7,8 +7,22 @@ def home(request):
     all_product=Product.objects.all().order_by('-created_at')
     return render(request,'home.html',{"products":all_product})
 
-# add product
 
+# add product
+def add_product(request):
+    if request.method=="POST":
+        product=Product()
+        product.product=request.POST.get('product')
+        product.purchase=request.POST.get('purchase')
+        product.sale=request.POST.get('sale')
+        product.qty=request.POST.get('qty')
+        product.gender=request.POST.get('gender')
+        product.note=request.POST.get('note')
+        product.save()
+        return HttpResponseRedirect('/')
+    else:
+        return render(request,'add.html')
+        
 # view the product individually
 
 # delete product
